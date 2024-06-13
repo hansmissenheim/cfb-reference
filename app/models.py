c9efeedb1b7e47f37e71c270d042e6ee20dd3f4c
Hans Missenheim <hansmissenheim@gmail.com>
2024-06-13 15:10:40 -0400

    Remove update models for now

--- app/models.py
+++ app/models.py
@@ -9,19 +9,7 @@ class Player(SQLModel, table=True):
     year: int
 
 
-class PlayerUpdate(Player):
-    first_name: str | None = None
-    last_name: str | None = None
-    position: int | None = None
-    year: int | None = None
-
-
 class School(SQLModel, table=True):
     id: int = Field(primary_key=True)
     name: str
     nickname: str
-
-
-class SchoolUpdate(School):
-    name: str | None = None
-    nickname: str | None = None

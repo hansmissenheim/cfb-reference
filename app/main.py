9921e80860daef771d4da287cdeb4e8fbc1a53b5
Hans Missenheim <hansmissenheim@gmail.com>
2024-06-13 12:34:22 -0400

    Create database with models on app startup

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from .database import create_db_and_tables
+from .models import Player, School
+
+
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
 def index():

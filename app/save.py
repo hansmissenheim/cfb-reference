e22bdd301fe30588525a4cbe5319f8235c53d095
Hans Missenheim <hansmissenheim@gmail.com>
2024-06-13 16:07:34 -0400

    Add creating teams to load_schools

--- app/save.py
+++ app/save.py
@@ -2,7 +2,7 @@ import ncaadb
 from sqlmodel import Session
 
 from .database import engine
-from .models import Player, School
+from .models import Player, School, Team
 
 
 def read_save():
@@ -13,13 +13,18 @@ def read_save():
 
 def load_schools(save_data):
     team_data = save_data["TEAM"]
+    seai_data = save_data["SEAI"]
+    year = 2023 + seai_data.at[0, "SSYE"]
+
     schools = [
         School(id=row.TGID, name=row.TDNA, nickname=row.TMNA)
         for row in team_data.itertuples()
     ]
+    teams = [Team(year=int(year), school_id=school.id) for school in schools]
 
     with Session(engine) as session:
         session.add_all(schools)
+        session.add_all(teams)
         session.commit()
 
 

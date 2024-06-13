bc1ad72d0e0db8b2d8fe15bf926c199b1fc08aa4
Hans Missenheim <hansmissenheim@gmail.com>
2024-06-13 16:56:14 -0400

    Add routes for retrieving all of a model type

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from sqlmodel import Session, select
 
-from .database import create_db_and_tables
+from .database import create_db_and_tables, engine
 from .models import Player, School, Team
 from .save import load_players, load_schools, read_save
 
@@ -14,3 +15,21 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/schools/")
+def read_schools():
+    with Session(engine) as session:
+        return session.exec(select(School)).all()
+
+
+@app.get("/teams/")
+def read_teams():
+    with Session(engine) as session:
+        return session.exec(select(Team)).all()
+
+
+@app.get("/players/")
+def read_players():
+    with Session(engine) as session:
+        return session.exec(select(Player)).all()

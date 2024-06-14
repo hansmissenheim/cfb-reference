7a9ba3dfb75cd36105604a8809971e701ce4e80e
Hans Missenheim <hansmissenheim@gmail.com>
2024-06-13 21:40:30 -0400

    Update database entries when already exists

--- app/save.py
+++ app/save.py
@@ -14,40 +14,60 @@ def read_save():
 def load_schools(save_data):
     team_data = save_data["TEAM"]
     seai_data = save_data["SEAI"]
-    year = 2023 + seai_data.at[0, "SSYE"]
-
-    schools = [
-        School(id=row.TGID, name=row.TDNA, nickname=row.TMNA)
-        for row in team_data.itertuples()
-    ]
-    teams = [Team(year=int(year), school_id=school.id) for school in schools]
+    year = 2023 + int(seai_data.at[0, "SSYE"])
 
     with Session(engine) as session:
-        session.add_all(schools)
-        session.add_all(teams)
+        for row in team_data.itertuples():
+            school = session.exec(select(School).where(School.id == row.TGID)).first()
+
+            if school is None:
+                school = School(id=row.TGID, name=row.TDNA, nickname=row.TMNA)
+                session.add(school)
+            else:
+                school.name = row.TDNA
+                school.nickname = row.TMNA
+
+            team = session.exec(
+                select(Team).where(Team.school_id == row.TGID).where(Team.year == year)
+            ).first()
+
+            if team is None:
+                team = Team(year=year, school_id=row.TGID)
+                session.add(team)
+
         session.commit()
 
 
 def load_players(save_data):
     player_data = save_data["PLAY"]
     seai_data = save_data["SEAI"]
-    year = 2023 + seai_data.at[0, "SSYE"]
+    year = 2023 + int(seai_data.at[0, "SSYE"])
 
     with Session(engine) as session:
         for row in player_data.itertuples():
             team = session.exec(
-                select(Team)
-                .where(Team.school_id == row.TGID)
-                .where(Team.year == int(year))
+                select(Team).where(Team.school_id == row.TGID).where(Team.year == year)
             ).first()
 
-            player = Player(
-                id=row.PGID,
-                first_name=row.PFNA,
-                last_name=row.PLNA,
-                position=row.PPOS,
-                year=row.PYEA,
-                teams=[team],
-            )
-            session.add(player)
+            player = session.exec(select(Player).where(Player.id == row.PGID)).first()
+
+            if player is None:
+                player = Player(
+                    id=row.PGID,
+                    first_name=row.PFNA,
+                    last_name=row.PLNA,
+                    position=row.PPOS,
+                    year=row.PYEA,
+                    teams=[team],
+                )
+                session.add(player)
+            else:
+                player.first_name = row.PFNA
+                player.last_name = row.PLNA
+                player.position = row.PPOS
+                player.year = row.PYEA
+
+                if team not in player.teams:
+                    player.teams.append(team)
+
         session.commit()

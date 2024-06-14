7ff076779aa53c58dbdc717d63237c48b9f18940
Hans Missenheim <hansmissenheim@gmail.com>
2024-06-13 20:40:37 -0400

    Create function to yield new db session

--- app/database.py
+++ app/database.py
@@ -1,4 +1,6 @@
-from sqlmodel import SQLModel, create_engine
+from collections.abc import Generator
+
+from sqlmodel import Session, SQLModel, create_engine
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -9,3 +11,8 @@ engine = create_engine(sqlite_url, connect_args=connect_args)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+
+def get_db() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session

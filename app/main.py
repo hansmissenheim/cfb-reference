ef6d37d95ae348192591b664c48d5f993ed36cef
Hans Missenheim <hansmissenheim@gmail.com>
2024-06-15 15:48:59 -0400

    Remove database loading on application start

--- app/main.py
+++ app/main.py
@@ -5,14 +5,10 @@ from app.api.main import api_router
 from app.core.config import settings
 
 from .database import create_db_and_tables
-from .save import load_players, load_schools, read_save
 
 
 async def lifespan(app: FastAPI):
     create_db_and_tables()
-    save_data = read_save()
-    load_schools(save_data)
-    load_players(save_data)
     yield
 
 

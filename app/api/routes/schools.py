96fac0a3174876004a5e2a1fc8514ce2354e79a5
Hans Missenheim <hansmissenheim@gmail.com>
2024-06-18 17:23:58 -0400

    Return Team model data with request

--- app/api/routes/schools.py
+++ app/api/routes/schools.py
@@ -5,7 +5,7 @@ from sqlmodel import select
 
 from app.core.config import settings
 from app.database import SessionDep
-from app.models import School
+from app.models import School, Team
 
 router = APIRouter()
 templates = Jinja2Templates(settings.TEMPLATES_DIR)
@@ -27,6 +27,17 @@ def all_schools(request: Request):
     return templates.TemplateResponse("school_all.html", {"request": request})
 
 
-@router.get("/{school_name}/{team_year}", response_class=HTMLResponse)
-def team(request: Request, school_name: str, team_year: int):
-    return templates.TemplateResponse("team.html", {"request": request})
+@router.get("/{school_slug}/{year}", response_class=HTMLResponse)
+def team(request: Request, school_slug: str, year: int, session: SessionDep):
+    school = session.exec(
+        select(School).where(School.url_slug == school_slug)
+    ).one_or_none()
+    if school is None:
+        raise HTTPException(status_code=404, detail="School not found")
+    team = session.exec(
+        select(Team).where(Team.school_id == school.id, Team.year == year)
+    ).one_or_none()
+    if team is None:
+        raise HTTPException(status_code=404, detail="Team not found")
+    context = {"request": request, "team": team}
+    return templates.TemplateResponse("team.html", context)

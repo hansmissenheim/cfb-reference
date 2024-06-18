1157cd3ee8cb5cb54d70755a3c8a6fecf46f9d51
Hans Missenheim <hansmissenheim@gmail.com>
2024-06-18 17:12:38 -0400

    Return School model data with request

--- app/api/routes/schools.py
+++ app/api/routes/schools.py
@@ -1,16 +1,25 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from sqlmodel import select
 
 from app.core.config import settings
+from app.database import SessionDep
+from app.models import School
 
 router = APIRouter()
 templates = Jinja2Templates(settings.TEMPLATES_DIR)
 
 
-@router.get("/{school_name}", response_class=HTMLResponse)
-def school(request: Request, school_name: str):
-    return templates.TemplateResponse("school.html", {"request": request})
+@router.get("/{school_slug}", response_class=HTMLResponse)
+def school(request: Request, school_slug: str, session: SessionDep):
+    school = session.exec(
+        select(School).where(School.url_slug == school_slug)
+    ).one_or_none()
+    if school is None:
+        raise HTTPException(status_code=404, detail="School not found")
+    context = {"request": request, "school": school}
+    return templates.TemplateResponse("school.html", context)
 
 
 @router.get("/", response_class=HTMLResponse)

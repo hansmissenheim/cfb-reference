122d5d31ea0f5d339308a599961213a2f1f961ba
Hans Missenheim <hansmissenheim@gmail.com>
2024-06-19 21:46:57 -0400

    Use SQLModel from models for alembic

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,5 @@
+from sqlmodel import SQLModel
+
 from app.models.links import PlayerSchoolLink, PlayerTeamLink
 from app.models.player import Player, PlayerAttributes
 from app.models.school import Coach, School, Stadium, Team

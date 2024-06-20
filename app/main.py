2968bdff2fbb47aa11ba5d9d185a74a3143b9ccc
Hans Missenheim <hansmissenheim@gmail.com>
2024-06-19 22:00:54 -0400

    Remove SQLModel db handling for alembic

--- app/main.py
+++ app/main.py
@@ -4,15 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from app.api.main import api_router
 from app.core.config import settings
 
-from .database import create_db_and_tables
-
-
-async def lifespan(app: FastAPI):
-    create_db_and_tables()
-    yield
-
-
-app = FastAPI(lifespan=lifespan)  # type: ignore
+app = FastAPI()
 app.mount("/static", StaticFiles(directory=settings.STATIC_DIR), name="static")
 
 app.include_router(api_router)

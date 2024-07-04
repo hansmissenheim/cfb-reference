2f73feb4c6625e560f638a228b75543c4e18b816
Hans Missenheim <hansmissenheim@gmail.com>
2024-07-04 02:26:39 -0400

    Add LoaderManager to handle model loaders

--- app/loading/main.py
+++ app/loading/main.py
@@ -1,6 +1,19 @@
 from typing import BinaryIO
 
+import ncaadb
 from sqlmodel import Session
+
+LOADERS = []
+
+
+class LoaderManager:
+    def __init__(self, save_file: BinaryIO, session: Session):
+        self.session = session
+        self.save_data = ncaadb.read_db(save_file)
+
+    def load_all(self):
+        for loader_class in LOADERS:
+            loader_class.load()
 
 
 def load_save_file(save_file: BinaryIO, session: Session) -> None: ...
